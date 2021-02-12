005d4fdc1c7e69c6af4ecf1e27a74d4f436e8265
Calvin Tang <tang.calvin1@gmail.com>
2021-02-12 10:52:40 -0500

    Set up navigation

---
+++ GithubFollowers/FavoritesListVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesListVC.swift
+//  GithubFollowers
+//
+//  Created by Calvin Tang on 2/12/21.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+}
--- GithubFollowers/SearchVC.swift
+++ GithubFollowers/SearchVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchVC.swift
 //  GithubFollowers
 //
 //  Created by Calvin Tang on 2/12/21.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemRed
     }
 
-
 }
-
