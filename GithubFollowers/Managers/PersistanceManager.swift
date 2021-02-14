d18d0f234764a910e09f6adddd2339b1179be41c
Calvin Tang <tang.calvin1@gmail.com>
2021-02-14 11:18:27 -0500

    Add persistance for followers

---
+++ GithubFollowers/Managers/PersistanceManager.swift
@@ -0,0 +1,77 @@
+//
+//  PersistanceManager.swift
+//  GithubFollowers
+//
+//  Created by Calvin Tang on 2/14/21.
+//
+
+import Foundation
+
+enum PersistenceActionType {
+    case add, remove
+}
+
+// Used an enum instead of struct since all properties inside are static and not reliant on an instance
+enum PersistenceManager {
+    
+    static private let defaults = UserDefaults.standard
+    
+    enum Keys {
+        static let favorites = "favorites"
+    }
+    
+    static func updateWith(favorite: Follower, actionType: PersistenceActionType, completed: @escaping (GFError?) -> Void) {
+        retrieveFavorites { (result) in
+            switch result {
+            case .success(let favorites):
+                var retrievedFavorites = favorites
+                
+                switch actionType {
+                case .add:
+                    // Check not favorited already
+                    guard !retrievedFavorites.contains(favorite) else {
+                        completed(.alreadyInFavorites)
+                        return
+                    }
+                    
+                    retrievedFavorites.append(favorite)
+                case .remove:
+                    retrievedFavorites.removeAll { $0.login == favorite.login}
+                }
+                
+                completed(save(favorites: retrievedFavorites))
+                
+            case .failure(let error):
+                completed(error)
+            }
+        }
+    }
+    
+    static func retrieveFavorites(completed: @escaping (Result<[Follower], GFError>) -> Void) {
+        guard let favoritesData = defaults.object(forKey: Keys.favorites) as? Data else {
+            // No favorites saved, return empty
+            completed(.success([]))
+            return
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            let followers = try decoder.decode([Follower].self, from: favoritesData)
+            completed(.success(followers))
+        } catch {
+            completed(.failure(.unableToFavorite))
+        }
+    }
+    
+    static func save(favorites: [Follower]) -> GFError? {
+        do {
+            let encoder = JSONEncoder()
+            let encodedFavorites = try encoder.encode(favorites)
+            defaults.set(encodedFavorites, forKey: Keys.favorites)
+            return nil
+        } catch {
+            return .unableToFavorite
+        }
+    }
+    
+}
